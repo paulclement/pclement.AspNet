a6e2b678bac21c3a07d7f5a8f45e9da44bd561c6
Paul CLEMENT <paul.clement.pro@hotmail.fr>
2015-12-24 01:11:41 +0100

    Init

---
+++ src/pclement.AspNet/Middlewares/RequestLoggerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pclement.AspNet.Middlewares
+{
+    public class RequestLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestLoggerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestLoggerMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation("Handling request: " + context.Request.Path);
+            if (context.Request.Path.Value.Contains("testMiddleware"))
+            { 
+                //await _next.Invoke(context);
+                _logger.LogInformation("updated");
+                await context.Response.WriteAsync("Hello World from RequestLoggerMiddleware!");
+            }
+            _logger.LogInformation("Finished handling request.");
+        }
+    }
+
+    public static class RequestLoggerExtensions
+    {
+        public static IApplicationBuilder UseRequestLogger(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggerMiddleware>();
+        }
+    }
+}
